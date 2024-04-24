34cc36b4ad7ed653995a9c36c5fd749a3d1b81bf
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-04-24 09:15:15 -0400

    chore: chases p6common refactor (#4)

--- lib/util.sh
+++ lib/util.sh
@@ -9,10 +9,10 @@
 p6df::modules::proxy::util::off() {
 
   local ev
-  for ev in $(p6_env_list | p6_filter_select "proxy=" "insensitive"); do
-    local e=$(p6_echo $ev | p6_filter_pluck_column "1" "=")
-    p6_echo $e
-    unset $e
+  for ev in $(p6_env_list | p6_filter_row_select "proxy=" "insensitive"); do
+    local e=$(p6_echo "$ev" | p6_filter_column_pluck "1" "=")
+    p6_echo "$e"s
+    unset "$e"
   done
 
   p6_return_void
